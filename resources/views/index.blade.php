d82af5eb7d8341d35594804e96fccec7bf689071
eduardoferreira20 <eduardoferreira@polijuniorengenharia.com.br>
2019-09-03 01:39:31 -0300

    site no heroku

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -197,43 +197,7 @@ body {
 		padding: 10px 0px 9px;
 	}
 </style>
-<!-- <div>
-<table style="float: left; font-style: 'Montserrat;'" class="table table-bordered">
-	<thead>
-		<tr>
-			<th>Evento</th>
-			<th>Data - Hora</th>
-			<th>Local</th>
-			<th>Cidade</th>
-			<th>Valor</th>
-			@auth('admin-web')
-			<th>Ação</th>
-			@endauth
-		</tr>
-	</thead>
-	<tbody>	
-		@foreach($events as $titulo)
-		<tr>
-			<td><a class="btn btn-link" href="{{route('events.show',$titulo->id)}}">{{$titulo->title}}</a></td>
-			<td>{{date('d/m - H:m', strtotime($titulo->start_date))}}</td>
-			<td>{{$titulo->local}}</td>
-			<td>{{$titulo->cidade}}</td>
-			@if($titulo['valor'] != 0)
-			<td>R$ {{$titulo->valor}}</td>
-			@else
-			<td>grátis</td>
-			@endif
-			@auth('admin-web')
-			<td>
-				<a class="btn btn-danger" href="javascript:(confirm('Deletar esse vento?') ? window.location.href='{{route('events.deletar', $titulo->id)}}' : false)">Deletar</a>
-				<a class="btn btn-info" href="{{route('events.edit',$titulo->id)}}">Editar</a>
-			</td>
-			@endauth
-		</tr>
-		@endforeach
-	</tbody>
-</table>
-</div> -->
+
 <br>
 <br>
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style=" overflow: auto !important;">
@@ -290,7 +254,7 @@ body {
 										<tr>
 											<td>
 												@auth('admin-web')
-												<a class="btn btn-danger" href="javascript:(confirm('Deletar esse vento?') ? window.location.href='{{route('events.deletar', $titulo->id)}}' : false)">Deletar</a>
+												<a class="btn btn-danger" href="javascript:(confirm('Deletar esse evento?') ? window.location.href='{{route('events.deletar', $titulo->id)}}' : false)">Deletar</a>
 												@endauth
 												<a class="btn btn-info" href="{{route('events.edit',$titulo->id)}}" style="">Acesse</a>
 											</td>
