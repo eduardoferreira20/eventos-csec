4dd1cdc4345bd11d7e745dad2020d8831acf0097
eduardoferreira20 <eduardoferreira@polijuniorengenharia.com.br>
2019-09-03 09:59:16 -0300

    erro ao mander email

--- app/Mail/SendEmailUser.php
+++ app/Mail/SendEmailUser.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendEmail extends Mailable
+class SendEmailUser extends Mailable
 {
     use Queueable, SerializesModels;
 
