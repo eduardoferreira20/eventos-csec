1d288a0f685678501f8ee292b876e76e847681f4
eduardoferreira20 <eduardoferreira@polijuniorengenharia.com.br>
2019-09-25 17:31:09 -0300

    scarn qr e admin page

--- app/Http/Controllers/InscricaoController.php
+++ app/Http/Controllers/InscricaoController.php
@@ -84,7 +84,7 @@ class InscricaoController extends Controller
 		$to_name =Auth::user()->name;
 		$user_name =Auth::user()->name;
 		$event = $eventos->title;
-		$to_email = Auth::user()->email;
+		$to_email = "dex@poli.br";
 
 		
 		$data = array('email'=>$to_email,'name'=>$event ,'title'=>$user_name,"body" => "Certificado do evento");
